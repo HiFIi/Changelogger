347439120214068af13db5a30c10be4596ea82a1
MBQ <mbqsniper@gmail.com>
2014-08-16 12:59:26 -0700

    ChangelogGUI - version 0.0.2a:
    * Fix some spelling
    * Add a tip to repo sync before anything
    * Add pound signs on the sides of the initial startup screen
    * Small UI changes
    * Version bump (duh)
    * Formatting

--- ChangelogGUI/ChangelogGUI.sh
+++ ChangelogGUI/ChangelogGUI.sh
@@ -1,234 +1,242 @@
 #!/bin/bash
 # To run this, simply cd to your ROM folder and run: 
-# . build/ChangelogGUI.sh
-# :)
+	# . build/ChangelogGUI.sh
+	# :)
 
-# Yes, I know my formatting is garbage. :p
+	# Variables (in case I need 'em)
+			TodaysDate=$(date +"%m-%d-%Y")
+			ChangelogFile=Changelogs/$date/ROM-Changelog-$date.txt
+			Changelogger=repo forall -pc git log --reverse --no-merges --since=$NumberOf.days.ago > $ChangelogFile
 
+			# Functions, yay!
 
-# Variables (in case I need 'em)
-TodaysDate=$(date +"%m-%d-%Y")
-ChangelogFile=Changelogs/$date/ROM-Changelog-$date.txt
-Changelogger=repo forall -pc git log --reverse --no-merges --since=$NumberOf.days.ago > $ChangelogFile
+			noMoreBoringStuff ()
+			{
+				echo " "
+				echo "Good news! We'll never have to do this again. :)"
+				echo "(Unless you delete the folder...)"
 
-# Functions, yay!
+				clear
+			}
 
-noMoreBoringStuff ()
-{
-echo " "
-echo "Good news! We'll never have to do this again. :)"
-echo "(Unless you delete the folder...)"
+			changelogFolder ()
+			{
+				# clear
 
-clear
-}
+				if [ ! -d Changelogs ]
+						then sleep 1.5
+						echo "First time?"
+						sleep 1.5
+						# echo " "
+						# echo " "
+						echo " "
+						echo "Creating Changelog folder..."
+						sleep 2
+						mkdir Changelogs
+						sleep 2
+						echo " "
+						echo "Done!"
+						sleep 1.3
+						echo " "
 
-changelogFolder ()
-{
-# clear
+						# Let the user know what's going on
+						echo "Adding Read and Write permissions to the folder..."
+						sleep 1.6
+						chmod 777 -R Changelogs
+						echo "Done!"
+						echo "Moving on..."
+						sleep 1.5
 
-if [ ! -d Changelogs ]
-then sleep 1.5
-echo "First time?"
-sleep 1.5
-# echo " "
-# echo " "
-echo " "
-echo "Creating Changelog folder..."
-sleep 2
-mkdir Changelogs
-sleep 2
-echo " "
-echo "Done!"
-sleep 1.3
-echo " "
+						# Now that all the boring setup stuff is done, let's let the user know.
+						noMoreBoringStuff
 
-# Let the user know what's going on
-echo "Adding Read and Write permissions to the folder..."
-sleep 1.6
-chmod 777 -R Changelogs
-echo "Done!"
-echo "Moving on..."
-sleep 1.5
+						else echo "Found the Changelog folder!"
+						sleep 1.5
+						echo " "
+						echo "Moving on..."
+						sleep 1.5
 
-# Now that all the boring setup stuff is done, let's let the user know.
-noMoreBoringStuff
+						fi
+			}
 
-else echo "Found the Changelog folder!"
-sleep 1.5
-echo " "
-echo "Moving on..."
-sleep 1.5
+			chmodFolder ()
+			{
+				echo " "
+				# echo " "
+				echo "Would you like to chmod 777 -R your Changelog folder now?"
+				echo "(Please enter yes, no, or help, followed by the ENTER key)"
+				echo " "
+				echo "Response:"
 
-fi
-}
+				read YesOrNo
 
-chmodFolder ()
-{
-echo " "
-# echo " "
-echo "Would you like to chmod 777 -R your Changelog folder now?"
-echo "(Please enter yes, no, or help, followed by the ENTER key)"
-echo " "
-echo "Response:"
+				if [ $YesOrNo == "yes" ]
+						echo " "
+						then echo "Nice! Doing that for you..."
+						sleep 1.6
+						chmod 777 -R Changelogs
+						echo "Done!"
+						echo "Moving on..."
+						sleep 1.5
 
-read YesOrNo
+						elif [ $YesOrNo == "no" ]
+								then echo "Okay then."
+								echo "Continuing..."
+								sleep 2
 
-if [ $YesOrNo == "yes" ]
-echo " "
-then echo "Nice! Doing that for you..."
-sleep 1.6
-chmod 777 -R Changelogs
-echo "Done!"
-echo "Moving on..."
-sleep 1.5
+								elif [ $YesOrNo == "help" ]
+										then echo "HELPING"
 
-elif [ $YesOrNo == "no" ]
-then echo "Okay then."
-echo "Continuining..."
-sleep 2
+										else
+											echo "WTF?"
+											fi
+			}
 
-elif [ $YesOrNo == "help" ]
-then echo "HELPING"
+			gitChangelog ()
+			{
+				echo "############################################################"
+				echo "#                                                          #"
+				echo "#                                                          #"
+				echo "#                      Date Chooser                        #"
+				echo "#                                                          #"
+				echo "#                                                          #"
+				echo "############################################################"		
+				echo " "
+				echo "How many days back would you like to go?"
+				echo "(enter the word 'today' if you'd like to pull the changes from today only)"
+				echo " "
+				echo "** Friendly tip! **"
+				echo "It's recommended to repo sync prior to pulling any changelogs"
+				echo " "
+				echo "Amount of days"
+				echo "--------------"
+				# echo " "
+				# echo " "
 
-else
-echo "WTF?"
-fi
-}
+				read NumberOf
 
-gitChangelog ()
-{
-# echo " "
-echo "How many days back would you like to go?"
-echo "(enter the word 'today' if you'd like to pull the changes from today only)"
-echo " "
-echo "Amount of days"
-echo "--------------"
-# echo " "
-# echo " "
+				if [ $NumberOf = $NumberOf ]
+						then echo " "
+						echo "Creating directory for todays date..."
+						sleep 2
+						mkdir Changelogs/$(date +"%m-%d-%Y")
 
-read NumberOf
+						echo " "
+						echo "Entering the directory for $TodaysDate"
+						sleep 1.5
 
-if [ $NumberOf = $NumberOf ]
-then echo " "
-echo "Creating directory for todays date..."
-sleep 2
-mkdir Changelogs/$(date +"%m-%d-%Y")
+						cd Changelogs/$(date +"%m-%d-%Y")
 
-echo " "
-echo "Entering the directory for $TodaysDate"
-sleep 1.5
+						echo " "
+						echo "Pulling the changelog from $NumberOf days ago"
 
-cd Changelogs/$(date +"%m-%d-%Y")
+						repo forall -pc git log --reverse --no-merges --since=$NumberOf.days.ago > ROM-Changelog-$(date +"%m-%d-%Y").txt
+						echo " "
+						echo "Done!"
+						sleep 2
 
-echo " "
-echo "Pulling the changelog from $NumberOf days ago"
+						cd ..
+						cd ..
 
-repo forall -pc git log --reverse --no-merges --since=$NumberOf.days.ago > ROM-Changelog-$(date +"%m-%d-%Y").txt
-echo " "
-echo "Done!"
-sleep 2
+						echo " "
+						echo "Settings some permissions..."
+						sleep 1.5
 
-cd ..
-cd ..
+						chmod 777 -R Changelogs
+						echo "Done!"
+						cd ..
+						#cd Changelogs &&
+						# mkdir $Date
+						# cd Changelogs
+						# cd $Date
+						echo " "
+						# echo "Done!"
+						sleep 2.4
 
-echo "Settings some permissions..."
-sleep 1.5
+						echo " "
+						echo "Go ahead and head on over to your Changelogs/$TodaysDate folder to find the changelog."
+						sleep 3
+						echo " "
 
-chmod 777 -R Changelogs
-cd ..
-#cd Changelogs &&
-# mkdir $Date
-# cd Changelogs
-# cd $Date
-echo " "
-# echo "Done!"
-sleep 2.4
+						echo "Exiting..."
+						sleep 2
 
-echo " "
-echo "Go ahead and head on over to your Changelogs/$TodaysDate folder to find the changelog."
-sleep 3
-echo " "
+						elif [ $NumberOf = "today" ]
+								then echo " "
+								echo "Creating directory for todays date..."
+								sleep 2
+								mkdir Changelogs/$(date +"%m-%d-%Y")
 
-echo "Exiting..."
-sleep 2
+								echo " "
+								echo "Entering the directory for $TodaysDate"
+								sleep 1.5
 
-elif [ $NumberOf = "today" ]
-then echo " "
-echo "Creating directory for todays date..."
-sleep 2
-mkdir Changelogs/$(date +"%m-%d-%Y")
+								cd Changelogs/$(date +"%m-%d-%Y")
 
-echo " "
-echo "Entering the directory for $TodaysDate"
-sleep 1.5
+								echo " "
+								echo "Pulling the changelog from 1 day ago"
 
-cd Changelogs/$(date +"%m-%d-%Y")
+								repo forall -pc git log --reverse --no-merges --since=1.day.ago > ROM-Changelog-$(date +"%m-%d-%Y").txt
+								echo " "
+								echo "Done!"
+								sleep 2
 
-echo " "
-echo "Pulling the changelog from 1 day ago"
+								cd ..
+								cd ..
 
-repo forall -pc git log --reverse --no-merges --since=1.day.ago > ROM-Changelog-$(date +"%m-%d-%Y").txt
-echo " "
-echo "Done!"
-sleep 2
+								echo "Settings some permissions..."
+								sleep 1.5
 
-cd ..
-cd ..
+								chmod 777 -R Changelogs
+								cd ..
+								#cd Changelogs &&
+								# mkdir $Date
+								# cd Changelogs
+								# cd $Date
+								echo " "
+								echo "Done!"
+								sleep 1.5
 
-echo "Settings some permissions..."
-sleep 1.5
+								echo " "
+								echo "Go ahead and head on over to your Changelogs/$TodaysDate folder to find the changelog."
+								sleep 1.5
+								echo " "
 
-chmod 777 -R Changelogs
-cd ..
-#cd Changelogs &&
-# mkdir $Date
-# cd Changelogs
-# cd $Date
-echo " "
-echo "Done!"
-sleep 1.5
+								echo "Exiting..."
+								fi
 
-echo " "
-echo "Go ahead and head on over to your Changelogs/$TodaysDate folder to find the changelog."
-sleep 1.5
-echo " "
+			}
+			clear
 
-echo "Exiting..."
-fi
+			cd build
+			# echo " "
+			# echo " "
 
-}
-clear
+			echo "############################################################"
+			echo "#                                                          #"
+			echo "#                                                          #"
+			echo "#                      Changelogger                        #"
+			echo "#                                                          #"
+			echo "#                     Version 0.0.2a                       #"
+			echo "#                                                          #"
+			echo "#                                                          #"
+			echo "############################################################"
+			sleep 2
+			# echo " "
+			# echo " "
+			# echo " "
+			echo " "
 
-cd build
-# echo " "
-# echo " "
+			# Create the Changelog folder if we can't find it
+			changelogFolder
 
-echo "############################################################"
-echo "                                                            "
-echo "                                                            "
-echo "                                                            "
-echo "                        Changelog!                          "
-echo "                                                            "
-echo "                      Version 0.0.1a                        "
-echo "                                                            "
-echo "                                                            "
-echo "############################################################"
-sleep 2
-# echo " "
-# echo " "
-# echo " "
-echo " "
+			# Ask if they want to chmod the folder
+			# EDIT: August 16th, 2014:
+				# Meh. I think we should just chmod it for ease of purpose.
+				# I left the function in just in case. It still has some bugs to fix
 
-# Create the Changelog folder if we can't find it
-changelogFolder
+				# chmodFolder
+				# ^^^ That's the function that prompts the response
 
-# Ask if they want to chmod the folder
-# EDIT: August 16th, 2014:
-# Meh. I think we should just chmod it for ease of purpose.
-# I left the function in just in case. It still has some bugs to fix
-
-# chmodFolder
-# ^^^ That's the function that prompts the response
-
-# Time to get to the good stuff
-gitChangelog
+				# Time to get to the good stuff
+				gitChangelog
